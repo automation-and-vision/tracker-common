8715d115131d2a6b5fd049a462601b6aefb3abf8
yurick <yu.hmelyuk@solarlab.ru>
2024-09-27 20:58:01 +0300

    add implements for gRPC of issue service

---
+++ grpc/server/issue_server.go
@@ -0,0 +1,16 @@
+package server
+
+import (
+	"context"
+	"github.com/automation-and-vision/tracker-common/grpc/issue_grpc"
+)
+
+type IssueServer struct {
+	issue_grpc.UnimplementedIssueServiceServer
+}
+
+func (s IssueServer) OnProjectCreated(ctx context.Context, req *issue_grpc.CreateProjectRequest) (*issue_grpc.CreateProjectResponse, error) {
+	return &issue_grpc.CreateProjectResponse{
+		Response: "response",
+	}, nil
+}
